ad51aeee823ba3187429724dea67daa212ed02a2
RainOuO <demon4920579@gmail.com>
2022-07-31 02:03:45 +0800

    reduce.js add

--- reduce.js
+++ reduce.js
@@ -1,21 +1,33 @@
-const arry = [1, 10, 20, 19];
-const reduce = arry.reduce((item, value) => {
-    return item + value
-}, 0);
-console.log(reduce); 
-/////
-let array = [1, 10, 20, 19];
-let sum = 0;
-for (let i of array) {
-    sum += i;
-}
-console.log(sum);
-//////////////////////////// test 陣列合併 不確定請問老師這樣對嗎?
-const arr = [['a', 'b'], ['c', 'd'], ['e', 'f']];
-console.log(arr);
-//////////////
-let sums = '';
-for (let n of arr) {
-    sums += n;
-}
-console.log(sums.split(' ')); //字串轉陣列
+let arr = [
+    {
+      name: 'Alex',
+      like: '鍋燒意麵',
+      age: 18
+    },
+    {
+      name: 'Rose',
+      like: '薯條',
+      age: 24
+    },
+    {
+      name: 'Lisa',
+      like: '章魚燒',
+      age: 12
+    },
+  ];      function reduce(arr){
+          let sum = 0;
+          for (let i of arr) {
+              sum += i.age;
+            }
+            return sum
+          }
+          console.log(reduce(arr)); //age 相加=54
+          
+          //////////////////////////// test 陣列合併 不確定請問老師這樣對嗎?
+          const array = [['a', 'b'], ['c', 'd'], ['e', 'f']];
+          console.log(array);
+          let sums = '';
+          for (let n of array) {
+              sums += n;
+          }
+          console.log(sums.split(' ')); //字串轉陣列 印出['a,bc,de,f']
