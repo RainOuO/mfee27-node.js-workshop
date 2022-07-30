303c1d4c0e063fdb7f2440d37fbdf32b99db1260
RainOuO <demon4920579@gmail.com>
2022-07-31 02:03:23 +0800

    map.js add

--- map.js
+++ map.js
@@ -1,15 +1,13 @@
 const ary = [5000, 2500, 4000, 1300];
 let test = ary.map(function(num) {
     return num * 2;});
-console.log(test);
+console.log(test); //[ 10000, 5000, 8000, 2600 ]
 
 function map(ary) {
     let result = [];
     for (let i = 0; i < ary.length; i++) {
-        if (ary !== 10000) {
             result.push(ary[i] * 2);
-        }
     }
     return result;
 }
-console.log(map(ary));
+console.log(map(ary)); //[ 10000, 5000, 8000, 2600 ]
