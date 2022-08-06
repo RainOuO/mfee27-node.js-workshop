2cde8b651b0486aa4aa950601a073da30f6a2c8b
RainOuO <demon4920579@gmail.com>
2022-08-06 11:12:55 +0800

    callback

--- callback/callback1.js
+++ callback/callback1.js
@@ -16,24 +16,24 @@ console.log(`開始工作 at ${dt.toISOString()}`);
 // 執行成功: 完成工作 刷牙 at 2022-08-06T02:46:57.761Z
 // 執行成功: 完成工作 吃早餐 at 2022-08-06T02:47:02.761Z
 // 執行成功: 完成工作 寫功課 at 2022-08-06T02:47:05.761Z
-doWork('刷牙', 3000, function (err, data) {
+doWork("刷牙", 3000, function (err, data) {
   if (err) {
-    console.error('發生錯誤了', err);
+    console.error("發生錯誤了", err);
   } else {
-    console.log('執行成功:', data);
+    console.log("執行成功:", data);
+    doWork("吃早餐", 5000, function (err, data) {
+      if (err) {
+        console.error("發生錯誤了", err);
+      } else {
+        console.log("執行成功:", data);
+        doWork("寫功課", 3000, function (err, data) {
+          if (err) {
+            console.error("發生錯誤了", err);
+          } else {
+            console.log("執行成功:", data);z
+          }
+        });
+      }
+    });
   }
 });
-doWork('吃早餐', 5000, function (err, data) {
-    if (err) {
-      console.error('發生錯誤', err);
-    } else {
-      console.log('執行成功', data);
-    }
-  });
-  doWork('寫功課', 3000, function (err, data) {
-    if (err) {
-      console.error('發生錯誤', err);
-    } else {
-      console.log('執行成功', data);
-    }
-  });
